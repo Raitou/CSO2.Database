08d09f1df43b3bcdfd4ff0b2b69abd0962397983
Raitou <raitoukun@hotmail.com>
2022-09-03 13:18:47 +0800

    Initial Commit

---
+++ CSO2.Database.Core/Models/IGN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSO2.Database.Core.Models
+{
+    public class IGN
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public DateTime? Updated { get; set; }
+    }
+}
---
+++ CSO2.Database.Core/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSO2.Database.Core.Models
+{
+    [Index(nameof(Email), IsUnique = true)]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+
+        public string? IPAddress { get; set; }
+
+        [DefaultValue(0)]
+        public int LoginFailedCount { get; set; } = 0;
+
+        public IGN Ign { get; set; } = null!;
+
+        [Required]
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+
+        public DateTime? Updated { get; set; }
+
+        public DateTime? LastLoggedIn { get; set; }
+
+    }
+}
